db40d428ce1e4590a377ea1a39a75a0cb25b02e8
HIMT <himt823@gmail.com>
2023-10-30 21:53:50 -0300

    Controlador Index con Spring MVC

---
+++ src/main/java/himt/empleados/controlador/IndexControlador.java
@@ -0,0 +1,30 @@
+package himt.empleados.controlador;
+
+import himt.empleados.modelo.Empleado;
+import himt.empleados.servicio.EmpleadoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class IndexControlador {
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    EmpleadoServicio empleadoServicio;
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String iniciar(ModelMap modelo){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado) -> logger.info(empleado.toString()));
+        //Compartimos el modelo con la vista
+        modelo.put("empleados", empleados);
+        return "index"; // index.jsp
+    }
+}
